a7d506023b529ab5cc6a3a3b3ccb6df60d56a32b
jjeeunit <jjeeunit@gmail.com>
2022-02-18 20:50:33 +0900

    ajax fetch commit

--- login1_server.php
+++ login1_server.php
@@ -2,28 +2,38 @@
  ini_set( "display_errors", 1 );
  error_reporting( E_ALL );
  
- session_start();
+ session_start(); 
+
+
+
+// print_r($_POST);
+
+// exit;
 
 $Db = new Mysqli('localhost','je','jejedb79!!','je');
 $userid = isset($_POST['userid']) ? $_POST['userid'] : false;
+
 $pass1 = isset($_POST['pass1']) ? $_POST['pass1'] : false;
 $sql = "select * from wpdms where userid = '$userid'";
 
-function _json($msg= '', $err = true)
+function _json($msg= '', $err = true) 
 {
-    $json = array('err' => $err, 'msg'=> $msg);
+    $json = array('msg'=> $msg ,'err' => $err);
 
     return exit(json_encode($json, JSON_FORCE_OBJECT));
 
 }
 
+if (strlen($userid) < 4) return _json('아이디는 4자 이상');
+if (strlen($pass1) < 8) return _json('비밀번호는 8자 이상');
 
 if ((false === ($res = $Db->query($sql)))) return _json('회원정보를 찾을 수 없습니다.');
-if ($res->num_rows < 1) return _json('아이디가 일치하지 않습니다.');
+if ($res->num_rows < 1) return _json('회원정보를 찾을 수 없습니다.');
 $row = $res->fetch_assoc();
-if (true !== password_verify($pass1, $row['pass1'])) return _json('비밀번호가 일치하지 않습니다.');
+if (true !== password_verify($pass1, $row['pass1'])) return _json('회원정보를 찾을 수 없습니다.');
 $_SESSION['userid'] = $userid;
 return _json('', false);
+
 
 /*
 if ((false === ($res = $Db->query($sql)))) exit(json_encode(array('err'=> true, 'msg' => '회원정보를 찾을 수 없습니다.')));
@@ -43,14 +53,3 @@ if ((false !== ($res = $Db->query($sql))) && $res->num_rows > 0 ) {
 }
 */
 ?>
-<html>
-<head>
-    <meta charset="UTF-8">
-</head>
-<body>
-    <br><br>
-    <div class="base">
-        <button type="button" name="btn" value="" onclick="location.href='logout1.php'">LOGOUT</button>
-    </div>
-</body>
-</html>
--- login1_view.php
+++ login1_view.php
@@ -19,10 +19,10 @@
     <script>
         var btn = document.getElementById('btn');
         btn.onclick = function() {
-            var str = document.getElementsByName('userid')[0].value;
-            var len = str.length;
-            var str2 = document.getElementsByName('pass1')[0].value;
-            var len2 = str2.length;
+            var str = document.getElementsByName('userid')[0].value,
+                len = str.length,
+                str2 = document.getElementsByName('pass1')[0].value,
+                len2 = str2.length;
             if(len < 4 ){
                 alert("아이디는 4자 이상입니다.");
                 return false;
@@ -31,32 +31,71 @@
                 alert("비밀번호는 8자 이상입니다.");
                 return false;
             }
-            return true;
-        }
- 
-    $(document).ready(function(){
-        $("#btn").click(function(){
-            
+            /**********fetch 사용 */
             var frm = document.login1_server;
-            
-            $.ajax({
-                url:'/login1_server.php',
-                type:'post',
-                datetype:'json',
-                data:{'userid':frm.userid.value, 'pass1':frm.pass1.value},
-                success:function(res){
-                    var json = $.parseJSON(res);
-                    
-                    if (json.err !== false) {
-                        alert(json.msg);
-                    } else {
-                        alert('로그인 성공');
-                        location.href="login1_server.php" ;
-                    }
+            var formData = new FormData(frm);
+        
+            fetch('./login1_server.php',{
+            method:"POST",
+            headers : new Headers(),
+            body : formData
+            })
+            .then((data) => {
+                if (data.err !== false) {
+                    var test = data.text();
+                    test.then(console.log);
+                    alert(data.text());
+                } else {
+                    alert('로그인 성공');
+                    location.href="login1_ajax.php";
                 }
             })
-        })
-    })
+            .catch(error=>console.log(error));
+            
+            
+            // /******참고 */
+            // var ajaxHeader = new Headers();
+            // fetch('/super/main/newpick.ajax.php', {
+            //     body : formData,
+            //     method :'post',
+            //     dataType : 'json',
+            //     headers : ajaxHeader
+            // }).then(function(response){
+            //     if (response.ok) return response.json();
+            //     else {
+            //         alert('error');
+            //     }
+            // }).then(function(json) {
+            //     if (json.err == true) {
+            //         alert(json.msg)
+            //     }else {
+            //         alert('정상적으로 ' + act + ' 되었습니다.');
+            //         location.reload();
+            //     }
+            // }).catch(function(error){
+            //     alert('System Error');
+            
+            // });
+            /**********아작스 사용*/
+            // var frm = document.login1_server;
+            
+            // $.ajax({
+            //     url:'./login1_server.php',
+            //     type:'post',
+            //     datetype:'json',
+            //     data:{'userid':frm.userid.value, 'pass1':frm.pass1.value},
+            //     success:function(res){
+            //         var json = $.parseJSON(res);
+                    
+            //         if (json.err !== false) {
+            //             alert(json.msg);
+            //         } else {
+            //             alert('로그인 성공');
+            //             location.href="login1_ajax.php";
+            //         }
+            //     }
+            // })
+        }
     </script>
 </body>
 </html>
